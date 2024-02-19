11bc2e9f51c385f0b6269b012327f409b2024cc0
Jsharp <jsharp56@ivytech.edu>
2024-02-18 21:35:56 -0500

    turn debug off

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a4e0e&$q_+wbw(p8h&2e98bf5new!%-8dh_v*n4fq6-c$74qq4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 

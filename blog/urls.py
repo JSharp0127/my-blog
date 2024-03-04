48a7ef2ba169fc7392b5928f2f08eff4081ec60d
Jsharp <jsharp56@ivytech.edu>
2024-03-03 19:34:54 -0500

    Stuff

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    
 ]
